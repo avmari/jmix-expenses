6e69aa49d048bd435126e5f99b937aa84f5114d5
avmari <mariya.avdeeva251001@gmail.com>
2022-07-08 13:50:16 +0400

    reports for employee's expenses and reports for expenses of a whole subdivision

--- src/main/java/com/company/jmixexpenses/screen/employee/EmployeeBrowse.java
+++ src/main/java/com/company/jmixexpenses/screen/employee/EmployeeBrowse.java
@@ -14,38 +14,4 @@ import java.time.LocalDate;
 @UiDescriptor("employee-browse.xml")
 @LookupComponent("employeesTable")
 public class EmployeeBrowse extends StandardLookup<Employee> {
-    static private boolean isStartDatePicked = false;
-    static private boolean isEndDatePicked = false;
-    static private boolean isTableRowPicked = false;
-    static private Employee selectedEmployee;
-
-    @Autowired
-    private Button createReportBtn;
-
-    @Subscribe("startDate")
-    public void onStartDateValueChange(HasValue.ValueChangeEvent<LocalDate> event) {
-        isStartDatePicked = true;
-        if(isEndDatePicked && isTableRowPicked)
-            createReportBtn.setEnabled(true);
-    }
-
-    @Subscribe("endDate")
-    public void onEndDateValueChange(HasValue.ValueChangeEvent<LocalDate> event) {
-        isEndDatePicked = true;
-        if(isStartDatePicked && isTableRowPicked)
-            createReportBtn.setEnabled(true);
-    }
-
-    @Subscribe("employeesTable")
-    public void onEmployeesTableSelection(Table.SelectionEvent<Employee> event) {
-        selectedEmployee = event.getSource().getSingleSelected();
-        isTableRowPicked = true;
-        if(isStartDatePicked && isEndDatePicked)
-            createReportBtn.setEnabled(true);
-    }
-
-
-    @Subscribe("createReportBtn")
-    protected void onCreateReportButtonClick(Button.ClickEvent event) {
-    }
 }
--- src/main/java/com/company/jmixexpenses/screen/expense/ExpenseBrowse.java
+++ src/main/java/com/company/jmixexpenses/screen/expense/ExpenseBrowse.java
@@ -1,10 +1,21 @@
 package com.company.jmixexpenses.screen.expense;
 
+import io.jmix.core.FileRef;
+import io.jmix.ui.component.GroupTable;
 import io.jmix.ui.screen.*;
 import com.company.jmixexpenses.entity.Expense;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @UiController("Expense.browse")
 @UiDescriptor("expense-browse.xml")
 @LookupComponent("expensesTable")
 public class ExpenseBrowse extends StandardLookup<Expense> {
+    @Autowired
+    private GroupTable<Expense> expensesTable;
+    @Subscribe
+    public void onInit(InitEvent event) {
+        expensesTable.getColumn("document").setFormatter(value -> {
+            return ((FileRef)value).getFileName();
+        });
+    }
 }
